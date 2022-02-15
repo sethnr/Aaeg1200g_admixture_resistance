c251709dfb00a0ca0e47b2e0b3e018d834e15c84
seth redmond <seth.redmond@gmail.com>
2022-02-15 23:18:58 +0000

    AIM refinement - handling empty all-invs table

--- workflows/inversion_hunting/scripts/lostruct_refine_aims_global.R
+++ workflows/inversion_hunting/scripts/lostruct_refine_aims_global.R
@@ -193,7 +193,7 @@ if(!exists("allinvsnps")) {
     do.call("grid.arrange", c(callplots, ncol=1))
   dev.off()
 } else {
-  file.create(allinvsnps)
+  file.create(outsnps)
   file.create(outsnpspng)
   file.create(outcallspng)
 }
