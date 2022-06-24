b709bc5bac390a9b5f9c2134961f95d9d368ffce
seth redmond <seth@seths-mbp.wkstn.qmul.ac.uk>
2022-06-24 11:27:44 +0100

    improved inversion cluster finding

---
+++ workflows/inversion_hunting/scripts/assess_inversion_candidates.R
@@ -0,0 +1,166 @@
+
+library("tidyverse")
+library("lostruct")
+
+#library("patchwork")
+library("gridExtra")
+library("grid")
+
+library("getopt")
+
+opttab <- matrix(c("infile","i","1","character",
+                   "vcf","v","1","character",
+                   "country","c","1","character",
+                   "samples","s","1","character",
+                   "outfile","o","1","character",
+                   "blocksize","b","2","integer",
+                   "maxd","D","2","double",
+                   "maxss","S","2","double",
+                   "minbss","B","2","double"
+                      ),byrow=T,ncol=4)
+opt <- getopt(opttab)
+
+invcandfile <- opt$infile
+outfile <- opt$outfile
+#chrom <- opt$chr
+vcffile <- opt$vcf
+country <- opt$country
+sppfile <- opt$samples
+
+#default inversion validation criteria
+MAXD <- 0.25
+MAXSS <- 20
+MINBSS <- 0.95
+blocksize <- 5e5
+if (!is.null(opt$blocksize)  ) {blocksize <- opt$blocksize}
+if (!is.null(opt$maxd)  ) {MAXD <- opt$maxd}
+if (!is.null(opt$maxss) ) {MAXSS <- opt$maxss}
+if (!is.null(opt$minbss)) {MINBSS <- opt$minbss}
+#invcandfile <- "uganda_chr1_pvclust_single_uncentered_hier_P0.99_n1000.txt"
+
+chromname <- c("NC_035107.1","NC_035108.1","NC_035109.1")
+chromlen <- c(310827022,474425716,409777670)
+names(chromlen) <- chromname
+
+spptab <- read.table(sppfile,header=T, sep="\t")
+samples <- spptab$sample[spptab$country==country]
+write(paste("found",length(samples),"samples for",country),stderr())
+
+
+invcands <- read.table(invcandfile,header=T)
+invcands$end <- as.numeric((as.data.frame(strsplit(invcands$block,":"))[2,]))
+invcands$start <- invcands$end-blocksize
+invcands$chromname <- chromname[invcands$chrom]
+
+for(C in unique(invcands$cluster)) {
+  #write.table(invcands[invcands$cluster==C,c("chromname","start","end")],stderr())
+  #write(samples,stderr())
+  invsnps <- vcf_query(vcffile,
+                        samples=samples,
+                        regions=invcands[invcands$cluster==C,c("chromname","start","end")])
+  goodsnps <- invsnps[apply(invsnps,1,function(x) {!any(is.na(x))}),]
+  write(paste("found",nrow(goodsnps),"snps for",country,"cluster",C),stderr())
+
+  pca <- prcomp(t(goodsnps))
+
+  invpcs <- as.data.frame(pca$x[,c("PC1","PC2")])
+  invpcs$sample <- samples
+  invpcs <- merge(invpcs,spptab)
+  invpcs$inv <- C
+  #invpcs$chrom <- chr
+
+  if(exists("pcs")) {
+    pcs <- rbind(pcs,invpcs)
+  } else {
+    pcs <- invpcs
+  }
+}
+
+write("assessing PCAs as inversions",file=stderr())
+
+invsummary <- invcands %>% 
+                    group_by(cluster) %>% 
+                    mutate(size = length(block)*blocksize)  %>% 
+                    select(c("cluster","au","bp","meandist","lowdist","size")) %>%
+                    unique()
+
+#### assess PCA clusters
+if(exists("pcs")) {
+  pcs$inv <- factor(pcs$inv,levels=invsummary$cluster,ordered=T)
+  pcs$valid<-factor(NA,levels=c("aa","ab","bb"))
+
+  for(I in unique(pcs$inv)) {
+    pcsinv <- subset(pcs,inv==I)
+    kmpca <- kmeans(pcsinv[,c("PC1")],centers=3,nstart=20,iter.max=50)
+
+    clusters <- factor(kmpca$cluster)
+    clustorder <- order(kmpca$centers)
+
+    #calculate deviation of middle cluster from center point
+    centers <- kmpca$centers[clustorder]
+    delta <- ((centers[2] - centers[1])-(centers[3] - centers[2])) /
+      ((centers[3]-centers[1])/2)
+
+    #rename levels 0/1/2 based on pcorder
+    levels(clusters)[clustorder] <- c('aa','ab','bb')
+
+    n<-length(clusters)
+    wss <- kmpca$withinss[clustorder]
+    tot.wss <- kmpca$tot.withinss
+    bss <- kmpca$betweenss
+    tot.ss <- kmpca$totss
+
+    invpass <- ((abs(delta)<=MAXD) && ((tot.wss/n) <= MAXSS) && (bss/tot.ss)>=MINBSS)
+    write(paste("inv:",I,
+                #"\tctr:",paste(round(centers,1),collapse="/"),
+                #"\twss:",paste(round(wss,1),collapse="/"),
+                "\tbss/tot:",round(bss/tot.ss,2),
+                "\tss",round(tot.wss/n,2),
+                "d",round(abs(delta),2),
+                "\tpass:",invpass),file=stderr())
+
+    invsummary[invsummary$cluster==I,"valid"] <- invpass
+    invsummary[invsummary$cluster==I,"d"] <- round(abs(delta),2)
+    invsummary[invsummary$cluster==I,"mean_wss"] <- round(tot.wss/n,2)
+    invsummary[invsummary$cluster==I,"bss_tot"] <- round(bss/tot.ss/n,2)
+
+
+    if(invpass) {
+      pcs$valid[which(pcs$inv==I)] <- clusters
+    }
+  }
+}
+
+
+write(paste("writing",nrow(invsummary),"candidates"),file=stderr())
+write.table(invsummary,file=paste(outfile,"txt",sep="."),sep="\t",quote=F,col.names=T,row.names=F)
+
+
+
+
+
+
+write(paste("plotting",nrow(invsummary),"PCs"),file=stderr())
+
+clustplot <- ggplot(invcands,aes(x=pos,fill=meandist,y=as.factor(cluster))) + geom_tile() + ylab("cluster")
+
+if(exists("pcs")) {
+  saveRDS(pcs,file=paste(outfile,"pcs.Rds",sep="."))
+
+  invcols <- scale_color_manual(values=c("aa"="yellow","ab"="orange","bb"="red"),na.value = "dark grey")
+  ncol=round(sqrt(length(unique(pcs$inv))))
+
+  invpca <- ggplot(pcs,aes(x=PC1,y=PC2,color=valid)) + geom_point() + coord_fixed() + invcols +
+    facet_wrap("inv ~ .",ncol=ncol) + theme(legend.position = "none")
+
+  png(filename = paste(outfile,"png",sep="."),width=350,height=200,units="mm",res=400)
+  grid.arrange(clustplot, invpca, ncol=2)
+  invpca
+  dev.off()
+} else {
+  saveRDS(NA,file=outpcs)
+  # png(filename = paste(outfile,"png",sep="."),width=350,height=200,units="mm",res=400)
+  grid.arrange(clustplot, grid.rect(gp=gpar(col="white")), ncol=2)
+  # grid.arrange(distplot,  grid.rect(gp=gpar(col="white")), ncol=2)
+  # dev.off()
+}
---
+++ workflows/inversion_hunting/scripts/plot_inversion_aims.R
@@ -0,0 +1,90 @@
+
+library("tidyverse")
+library("lostruct")
+
+#library("patchwork")
+library("gridExtra")
+library("grid")
+
+library("getopt")
+
+opttab <- matrix(c("infile","i","1","character",
+                   "samples","s","1","character",
+                   "outfile","o","1","character"
+),byrow=T,ncol=4)
+opt <- getopt(opttab)
+
+invaimsfile <- opt$infile
+metafile <- opt$samples
+outsnpspng <- opt$outfile
+
+
+write("gathering meta",file=stderr())
+
+metatab <- read.table(metafile,header=T, sep="\t")
+metatab$contgroup <- factor(metatab$contgroup,levels=c("Wafrica","Safrica","Eafrica","Americas","Asia"),ordered=T)
+metatab$region <- factor(metatab$region,levels=c("East Africa","West Africa","South America","Carribean","North America","Middle East","Asia","Pacific"),ordered=T)
+
+countrysorttab <- unique(metatab[,c("country","contgroup","region")])
+metatab$country <- factor(metatab$country,levels=unique(metatab$country[order(metatab$region)]),ordered=T)
+samples <- metatab$sample
+
+
+# invcands <- read.table(invfile,header=T)
+# invcands <- subset(invcands,as.logical(valid))
+
+write("reading inv snps",file=stderr())
+
+if (file.size(invaimsfile)>0) {
+  allinvsnps <- read.table(invaimsfile,header=T,check.names = F)           
+  write.table(table(allinvsnps$inv),file=stderr(),row.names = F,quote=F,col.names = F)
+  write(paste(length(unique(allinvsnps$inv)),"invs found in",invaimsfile),file=stderr())
+  allinvnames <- unique(allinvsnps$inv)
+} else {
+  allinvnames <- c()
+}
+
+# mlpreds <- read.table(mlcallsfile,header=F,col.names = unique(allinvsnps$inv))           
+# meanpreds <- read.table(meancallsfile,header=T)           
+
+aimplots=list()
+for(invname in allinvnames) {
+    write(paste(" ",invname),file=stderr())
+    invnamesafe <- paste("X",gsub("\\D",".",invname,perl=T),sep="")
+    invsnps <- subset(allinvsnps,inv==invname)
+    
+    meansnp <- apply(invsnps[,samples],2,FUN=function(x) {mean(na.omit(x))})
+    
+    cntinvorder <- metatab$sample[order(metatab$contgroup,metatab$region,metatab$country,meansnp[metatab$sample])]
+    cntorder <- unique(metatab$country[order(metatab$contgroup,metatab$region)])
+    
+    aimsM <- pivot_longer(invsnps,all_of(samples),names_to = "sample") %>% rename("invcountry"="country")
+    aimsM <- merge(aimsM,metatab,by="sample")
+    aimsM$sample <- factor(aimsM$sample,levels = cntinvorder,ordered=T)
+    aimsM$country <- factor(aimsM$country,levels = cntorder,ordered=T)
+    aimsM$cncode <- aimsM$country
+    levels(aimsM$cncode) <- substr(levels(aimsM$country),0,3)
+
+    aimplot <- ggplot(aimsM,aes(x=i,y=as.numeric(sample),fill=as.factor(value))) + geom_raster() + 
+       ylab("samples") + xlab("SNPs")+ theme(legend.position="none")+
+       scale_y_continuous(expand = c(0,0)) + scale_x_continuous(expand = c(0,0)) +
+      scale_fill_manual(values=c("0"="blue","1"="purple","2"="red")) +
+      facet_grid("cncode ~ .",scale="free_y",space="free_y") +
+      ggtitle(paste(invname,"aim calls","( top",nrow(invsnps),")"))+
+      theme(panel.spacing = unit(0.2, "mm"),
+            axis.text.y=element_blank(),
+            axis.title.y=element_blank(),
+            axis.ticks.y=element_blank())
+    aimplots[[invnamesafe]] <- aimplot
+
+    
+    
+}
+
+if(length(aimplots)>0) {
+  print(length(aimplots),stderr())
+  png(outsnpspng,res=400,width=400,height=200,units='mm')
+    do.call("grid.arrange", c(aimplots, nrow=1))
+  dev.off()
+} else {file.create(outsnpspng)}
+
