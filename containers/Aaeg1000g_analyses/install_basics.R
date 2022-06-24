2886b7a3280d3bae64ed4d11842cb77622b38b43
seth redmond <seth@seths-mbp.wkstn.qmul.ac.uk>
2022-06-24 14:36:01 +0100

    updating rules for local run

--- containers/Aaeg1000g_analyses/install_basics.R
+++ containers/Aaeg1000g_analyses/install_basics.R
@@ -7,7 +7,7 @@ options(warn = 2)     # treat warnings as errors, otherwise can fail silently
 repos <- c('http://cran.us.r-project.org')
 
 # required = c("gridExtra","zoo","devtools")
-required = c("gridExtra","zoo","getopt","patchwork")
+required = c("gridExtra","zoo","getopt","patchwork","pvclust")
 install.packages(required,repos=repos,clean=TRUE)
 
 q(save = "no")
