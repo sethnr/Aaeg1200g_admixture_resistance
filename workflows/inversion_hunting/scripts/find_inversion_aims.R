d258004fce196ff6c1eb36ef604a634974b77b0e
seth redmond <seth@192.168.1.160>
2022-08-05 10:54:33 +0100

    bugfix for all NA vals in aims

--- workflows/inversion_hunting/scripts/find_inversion_aims.R
+++ workflows/inversion_hunting/scripts/find_inversion_aims.R
@@ -186,7 +186,7 @@ for(C in unique(invcands$cluster)) {
 
    #if inversely correlated with modal value, flip call
     modecall <- apply(invsnps,2,function(x) {as.numeric(names(sort(table(na.omit(x)),decreasing = T))[1])})
-    modecorr <- apply(invsnps,1,function(x) {cor(modecall[!is.na(x)],x[!is.na(x)])})
+    modecorr <- apply(invsnps,1,function(x) {if(sum(!is.na(x))>0) {cor(modecall[!is.na(x)],x[!is.na(x)])} else {0}})
     invsnps[modecorr<0,] <- abs(invsnps[modecorr<0,]-2)
 
 
