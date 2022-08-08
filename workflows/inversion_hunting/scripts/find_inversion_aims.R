07e1ae292cc63b22d9804232c2dee6e7a9372abe
seth redmond <seth@192.168.1.160>
2022-08-08 17:06:30 +0100

    bugfixing aims / merge scripts

--- workflows/inversion_hunting/scripts/find_inversion_aims.R
+++ workflows/inversion_hunting/scripts/find_inversion_aims.R
@@ -209,26 +209,30 @@ for(C in unique(invcands$cluster)) {
     invaims <- invaims[ldinclude,]
     invaims$i <- c(1:nrow(invaims))
 
-
-    write(paste(is.data.frame(invsnps),
+    write(paste(sum(ldinclude),
+                is.data.frame(invsnps),
                 is.matrix(invsnps),
                 is.data.frame(invsnps) | is.matrix(invsnps)),
                 stderr())
     if(is.data.frame(invsnps) | is.matrix(invsnps)) {
         write(paste("  -->",nrow(invsnps)),file=stderr())
         meancall <- apply(invsnps,2,function(x) {mean(na.omit(x))})
+        #order all SNPs by country, then mean inv call of high LD SNPs
+        cntinvorder <- metatab$sample[order(metatab$contgroup,metatab$country,meancall)]
+
     } else {
         write(paste("  --> one snp?"),file=stderr())
         meancall <- mean(na.omit(invsnps))
+        #order all SNPs by country, then mean inv call of high LD SNPs
+        cntinvorder <- metatab$sample[order(metatab$contgroup,metatab$country)]
     }
 
-    #order all SNPs by country, then mean inv call of high LD SNPs
-    cntinvorder <- metatab$sample[order(metatab$contgroup,metatab$country,meancall)]
     invaims$qual <- mean(abs(modecorr))
 
     write(dim(invaims),stderr())
     write(dim(invsnps),stderr())
     invsnps <- cbind(invaims,invsnps)
+
 
     if(!exists("allinvsnps")) {
         write(dim(invsnps),stderr())
@@ -236,7 +240,6 @@ for(C in unique(invcands$cluster)) {
     } else {
         write(dim(allinvsnps),stderr())
         write(dim(invsnps),stderr())
-
         allinvsnps <- rbind(allinvsnps,invsnps)}
 
 
