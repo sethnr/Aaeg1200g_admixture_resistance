e0cd07fa6a6f38facad8859ffe8f45b76a29be85
seth redmond <seth@192.168.1.101>
2022-08-18 15:39:20 +0100

    adding hetz to assessment

--- workflows/inversion_hunting/scripts/merge_inversions_by_linkage.R
+++ workflows/inversion_hunting/scripts/merge_inversions_by_linkage.R
@@ -72,7 +72,7 @@ if(!exists("allblocks")) {
         write(paste("writing empty files for",filename),stderr())
         file.create(filename)
     }
-    stop(paste("no inversion to merge for chr",chrom))
+    q("no",0,F)
 }
 
 
