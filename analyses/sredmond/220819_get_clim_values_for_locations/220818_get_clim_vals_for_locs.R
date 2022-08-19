9aac7e95719fc8107941e586af4d5ac061b15202
seth redmond <seth@192.168.1.101>
2022-08-19 21:14:57 +0100

    updating climate vals table

--- analyses/sredmond/220819_get_clim_values_for_locations/220818_get_clim_vals_for_locs.R
+++ analyses/sredmond/220819_get_clim_values_for_locations/220818_get_clim_vals_for_locs.R
@@ -4,11 +4,6 @@ library("patchwork")
 library("gridExtra")
 library("grid")
 
-library("genetics")
-
-library(maptools)
-library(ggmap)
-
 library("raster")
 
 
@@ -21,7 +16,6 @@ density <- raster(rasterfile)
 poplocs$popdens <- extract(density,poplocs[,c("long","lat")],method="bilinear")
 
 
-seasonality <- raster("resources/rasters/wc2.1_2.5m_bio_4.tif")
 maxtemp <- raster("resources/rasters/wc2.1_2.5m_bio_5.tif")
 mintemp <- raster("resources/rasters/wc2.1_2.5m_bio_6.tif")
 precipitation <- raster("resources/rasters/wc2.1_2.5m_bio_12.tif")
@@ -31,11 +25,11 @@ precseasonality <- raster("resources/rasters/wc2.1_2.5m_bio_15.tif")
 poplocs$maxtemp <- extract(maxtemp,poplocs[,c("long","lat")],method="bilinear")
 poplocs$mintemp <- extract(mintemp,poplocs[,c("long","lat")],method="bilinear")
 poplocs$precipitation <- extract(precipitation,poplocs[,c("long","lat")],method="bilinear")
-poplocs$precseason <- extract(seasonality,poplocs[,c("long","lat")],method="bilinear")
-poplocs$tempseason <- extract(seasonality,poplocs[,c("long","lat")],method="bilinear")
+poplocs$precseason <- extract(precseasonality,poplocs[,c("long","lat")],method="bilinear")
+poplocs$tempseason <- extract(tempseasonality,poplocs[,c("long","lat")],method="bilinear")
 
 
 colnames(poplocs)
 
-write.table(poplocs[,c("index","label","lat","long","popdens","maxtemp","mintemp","precipitation","precseason","tempseason")],
+write.table(poplocs[,c("index","pop","lat","long","popdens","maxtemp","mintemp","precipitation","precseason","tempseason")],
             "meta_loc_demo_clim_vals.txt",sep="\t",quote=F,row.names=F,col.names=T)
