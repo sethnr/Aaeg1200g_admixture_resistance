65b1dfc55165c56afca1d39e238acc810295719f
seth redmond <seth@192.168.1.101>
2022-08-19 09:26:14 +0100

    introducing min AIMs requirement

--- workflows/inversion_hunting/scripts/find_inversion_aims.R
+++ workflows/inversion_hunting/scripts/find_inversion_aims.R
@@ -183,7 +183,7 @@ for(C in unique(invblocks$inv)) {
         meanr2s <- apply(r2s,1,mean)
 
         if(is.na(MINR2)) {
-            write(paste("no MINR2 given, using mean-sd ("mean(r2s),sd(r2s),") for inv",C),stderr())
+            write(paste("no MINR2 given, using mean-sd (",mean(r2s),sd(r2s),") for inv",C),stderr())
             ldinclude <- meanr2s >= (mean(r2s)-sd(r2s))
         } else {
             ldinclude <- meanr2s >= MINR2}
