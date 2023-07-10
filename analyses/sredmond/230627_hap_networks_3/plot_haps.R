6f5fa41b0ce5ee9cfe795a6718342916bbde2049
seth redmond <seth@SNRm2blue.local>
2023-07-10 18:14:22 +0100

    adding hapnets 3 script

---
+++ analyses/sredmond/230627_hap_networks_3/plot_haps.R
@@ -0,0 +1,39 @@
+library(ape)
+library(pegas)
+library(vcfR)
+
+f <- "snpeff.kdr.nonsyn.vcf.gz"
+
+snps <- VCFloci(f)
+samples <- VCFlabels(f)
+
+v <- pegas::read.vcf(f)
+h <- pegas::haplotype(v,is.snp(snps))
+dh <- dist.haplotype.loci(h)
+
+x <- vcfR2DNAbin(read.vcfR(f, verbose = FALSE))
+h <- haplotype(x)
+d <- dist.dna(h,"n")
+
+# #min spanning tree
+# nt <- rmst(d)
+#minimum spanning network
+nt <- mst(d)
+
+
+meta <- read.table("meta_Aaeg1kg_spp.txt",header=T,sep="\t")
+rownames(meta) <- meta$sample
+
+R <- haploFreq(x,fac = rep(meta[samples,"region"],each=2),h)
+
+ntlabs <- attr(nt,"labels")
+
+sz <- summary(h)
+
+
+png(gsub(".vcf.gz",".png",f),width=800,height=800)
+plot(nt,fast=T,threshold=c(5,10),
+     pie=R[ntlabs,],size=sz[ntlabs]/100,
+     legend = c(0.15, 9.5))
+dev.off()
+
---
+++ analyses/sredmond/230627_hap_networks_3/plot_haps_geneHapR.R
@@ -0,0 +1,111 @@
+# if (!require("BiocManager", quietly = TRUE))
+#   install.packages("BiocManager")
+# BiocManager::install(version = "3.14")
+# 
+# 
+BiocManager::install("GenomicRanges",update=F)
+BiocManager::install("Biostrings",update=F)
+BiocManager::install("rtracklayer",update=F)
+BiocManager::install("trackViewer",update=F)
+install.packages("geneHapR")
+# 
+
+library("geneHapR")
+library("vcfR")
+library("pegas")
+library("getopt")
+library("tidyverse")
+
+opttab <- matrix(c("vcf","i","1","character",
+                   "out","o","1","character",
+                   "meta","m","1","character"
+),byrow=T,ncol=4)
+opt <- getopt(opttab)
+
+#f <-"locushap_shapeit/snpeff.kdr3.m0.01_noUTR.vcf.gz"
+#prefix <- "kdr3"
+
+
+vcffile <- opt$vcf
+prefix <- opt$out
+metafile <- opt$meta
+
+meta <- read.table(metafile,sep="\t",header=T)
+
+metahap <- rbind(meta %>% mutate(sample=paste(sample,"0",sep="_")),
+                 meta %>% mutate(sample=paste(sample,"1",sep="_")))
+rownames(metahap) <- metahap$sample
+
+poplocs <- read.table("resources/aegy.wgs.pops.list.csv",sep=",",header=T) %>% 
+                    select("Pop","Lat","Long") %>% 
+                    rename("pop"="Pop","poplat"="Lat","poplong"="Long")
+cntlocs <- read.table("resources/country_geocoords.txt",sep="\t",header=T) %>% 
+                    rename("cntlat"="lat","cntlong"="lon")
+
+
+metahaploc <- merge(merge(metahap,cntlocs,by="country"),
+                    poplocs,by="pop") %>% rename("Hap"="sample")
+rownames(metahaploc) <- metahaploc$Hap
+
+
+# vcf <- import_vcf(vcffile)
+# hapResult <- vcf2hap(vcf,
+#                      hapPrefix = "H",
+#                      hetero_remove = FALSE,
+#                      na_drop = T)
+
+#vcf <- pegas::read.vcf(vcffile)
+loci <- VCFloci(vcffile)
+
+vcfr <- read.vcfR(vcffile)
+gts <- extract.gt(vcfr)
+haps <- extract.haps(vcfr)
+
+#only include samples in haps file (removed related, etc)
+haps <- haps[,colnames(haps) %in% metahap$sample]
+
+#paste(rep(VCFlabels(vcffile),each=2),c("0","1"),sep="_")
+haptab <- cbind(loci[,c(1,2,4,5,8)],haps)
+
+hapResult <- table2hap(haptab[c(grep("MODERATE",haptab$INFO),grep("HIGH",haptab$INFO)),])
+
+
+hapSummary <- hap_summary(hapResult)
+
+W=300
+H=200
+R=400
+png(paste(prefix,"haptable.png",sep="_"),width=W,height=H,res=R,units="mm")
+#plot haplotype table
+plotHapTable(hapSummary)
+dev.off()
+
+
+#plot haplotype network
+hapNet <- get_hapNet(hapSummary,
+                     AccINFO = metahap,
+                     groupName = "region")
+
+
+png(paste(prefix,"hapnet.png",sep="_"),width=W,height=H,res=R,units="mm")
+plotHapNet(hapNet,
+           show.mutation = 2,
+           legend=c(10,-15),
+           show_size_legend=F,
+           scale='log2',
+           threshold=0)
+dev.off()
+
+png(paste(prefix,"geodist_pop.png",sep="_"),width=W,height=H,res=R,units="mm")
+hapDistribution(hapResult,metahaploc,"poplong","poplat",
+                c("H001","H002","H003","H004","H005"),
+                legend=TRUE)
+dev.off()
+
+png(paste(prefix,"geodist_cnt.png",sep="_"),width=W,height=H,res=R,units="mm")
+hapDistribution(hapResult,metahaploc,"cntlong","cntlat",
+                c("H001","H002","H003","H004","H005",
+                  "H006","H007","H008","H009","H010"),
+                legend=TRUE)
+dev.off()
+
